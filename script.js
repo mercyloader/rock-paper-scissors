caae21bd1fae88636e04c2ab196efeacc46ccc30
mercyloader <mercyloader1@gmail.com>
2026-07-15 16:26:17 +0100

    Add function that returns user choice

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 //Create a function that randomly returns
 //"Rock", "Paper" and "Scissors"
 function getComputerChoice() {
-    let any = Math.floor(Math.random() * 10);
-    if (any == 0 || any == 1 || any == 2 || any == 3) {
+    let computer = Math.floor(Math.random() * 10);
+    if (computer == 0 || computer == 1 || computer == 2 || computer == 3) {
         return "Rock";
-    } else if (any == 4 || any == 5 || any == 6) {
+    } else if (computer == 4 || computer == 5 || computer == 6) {
         return "Paper";
-    } else if (any == 7 || any == 8 || any == 9) {
+    } else if (computer == 7 || computer == 8 || computer == 9) {
         return "Scissors";
     } else {}
 }
+
+//Create a function that takes the user 
+//choice and returns it.
+function getHumanChoice() {
+    let human = prompt("Choose one: Rock, Paper or Scissors", "");
+    return human;
+}
+
